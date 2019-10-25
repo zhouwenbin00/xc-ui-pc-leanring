18261501e004cdccda59b8c80439c26cc1022158
zhouwenbin <2825075112@qq.com>
2019-10-25 16:31:40 +0800

    day13

---
+++ config/sysConfig.js
@@ -0,0 +1,12 @@
+var sysConfig = {
+    xcApiUrlPre: '/api',//客户端请求
+    staticURL: '/static',
+    backApiURL: 'http://www.demo.com/openapi',//服务端请求
+    backStaticURL: 'http://www.demo.com/static',
+    imgUrl:'http://img.demo.com/',
+    videoUrl:'http://video.demo.com/video/',
+    openAuthenticate:true,
+    openAuthorize:true
+}
+
+module.exports = sysConfig
